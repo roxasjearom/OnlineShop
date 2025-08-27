142f08c58b0487af6bf761c9caaec5cb5e488f97
Jearom Roxas <roxasjearom@gmail.com>
2025-08-27 12:15:50 +0800

    Upgrade Kotlin, CMP and AGP versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -40,6 +40,7 @@ kotlin {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
+            implementation(libs.compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
